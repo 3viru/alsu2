c5ad5178a3303a5fb76ba69cd915963e75756503
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-08-25 20:19:44 -0400

    actualize vehiculo.py

--- vehiculo.py
+++ vehiculo.py
@@ -3,3 +3,18 @@ class Vehiculo:
         self.patente: str = patente
         self.anio: int = anio
         self.__en_taller: bool = False
+
+    def ingresar(self) -> str:
+        if self.__en_taller:
+            return "el vehiculo ya esta aki"
+        self.__en_taller = True
+        return "vehiculo ha ingreso al taller"
+
+    def entregar(self) -> str:
+        if not self.__en_taller:
+            return "el vehiculo no se esta aki"
+        self.__en_taller = False
+        return "el vehiculo ha sidop entregao"
+    def tarifa_hora(self) -> int:
+        return 5000
+    
